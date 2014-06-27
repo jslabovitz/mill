7a7be3b142f5991c27123973ffe97eea2c59c7a1
John Labovitz <johnl@johnlabovitz.com>
2014-06-27 09:44:15 -0400

    Treat hidden files as 404s. Remove unused requirment on our Path extensions. Change log levels from 'info' to 'debug'. Fix typo in log message.

--- lib/mill/server.rb
+++ lib/mill/server.rb
@@ -3,9 +3,6 @@ require 'addressable/uri'
 require 'path'
 require 'pp'
 
-$LOAD_PATH.unshift "#{ENV['HOME']}/Projects/mill/lib"
-require 'mill/extensions/path'
-
 class Mill
 
   class Server < Sinatra::Application
@@ -15,21 +12,21 @@ class Mill
       path = Path.new(settings.public_dir) / Path.new(uri_path).relative_to('/')
       path /= 'index' if uri_path[-1] == '/'
       if path.directory?
-        log.info "#{uri_path} => [redirect] #{uri_path}"
+        log.debug "#{uri_path} => [redirect] #{uri_path}"
         return redirect "#{uri_path}/"
-      elsif path.file?
-        log.info "#{uri_path} => [file] #{path}"
+      elsif path.file? && path.basename.to_s != '.'
+        log.debug "#{uri_path} => [file] #{path}"
         return send_file(path)
       elsif path.extname.empty?
         %w{.html .jpg}.each do |extname|
           p = path.add_extension(extname)
           if p.file?
-            log.info "#{uri_path} => [file] #{p}"
+            log.debug "#{uri_path} => [file] #{p}"
             return send_file(p)
           end
         end
       end
-      log.info "#{uri_path} => [not found]}"
+      log.debug "#{uri_path} => [not found]"
       halt 404, 'not found'
     end
 
