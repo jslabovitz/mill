9225267aac1a8cb0f291b674afcf34bb83a35fc4
John Labovitz <johnl@johnlabovitz.com>
2016-02-20 17:21:12 -0500

    Revert "Update Google Analytics script."
    This reverts commit fa02415f16072eed618136faa3faf9bdc15a7dd8.

--- lib/mill/html_helpers.rb
+++ lib/mill/html_helpers.rb
@@ -130,12 +130,16 @@ module HTMLHelpers
     html_fragment do |html|
       html.script(type: 'text/javascript') do
         html << %Q{
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', '#{tracker_id}', 'auto');
-  ga('send', 'pageview');
+          var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+          document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        }
+      end
+      html.script(type: 'text/javascript') do
+        html << %Q{
+          try {
+            var pageTracker = _gat._getTracker("#{tracker_id}");
+            pageTracker._trackPageview();
+          } catch(err) {}
         }
       end
     end
