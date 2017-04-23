a71b3b33d31f0e4115373a75cd8e373f159dbb89
John Labovitz <johnl@johnlabovitz.com>
2017-04-23 10:16:30 -0400

    Allow some site attributes to be missing.

--- lib/mill/resources/feed.rb
+++ lib/mill/resources/feed.rb
@@ -15,14 +15,14 @@ module Mill
             xml.id(@site.tag_uri)
             xml.generator(*@site.feed_generator)
             xml.title(@site.site_title) if @site.site_title
-            xml.link(rel: 'alternate', type: 'text/html',             href: @site.home_resource.uri)
+            xml.link(rel: 'alternate', type: 'text/html',             href: @site.home_resource.uri) if @site.home_resource
             xml.link(rel: 'self',      type: 'application/atom+xml',  href: uri)
             xml.author do
               xml.name(@site.feed_author_name) if @site.feed_author_name
               xml.uri(@site.feed_author_uri) if @site.feed_author_uri
               xml.email(@site.feed_author_email) if @site.feed_author_email
             end
-            xml.updated(resources.last.date.iso8601)
+            xml.updated(resources.last.date.iso8601) unless resources.empty?
             resources.each do |resource|
               xml.entry do
                 xml.title(resource.title) if resource.title
