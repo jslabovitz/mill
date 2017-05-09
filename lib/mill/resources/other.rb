bee2306596dbaa9da0a41a464f4285f8e8846872
John Labovitz <johnl@johnlabovitz.com>
2017-05-09 10:05:38 -0400

    Merge Resource::Font, ::Javascript, and ::RTF into ::Other.

--- lib/mill/resources/other.rb
+++ lib/mill/resources/other.rb
@@ -6,7 +6,20 @@ module Mill
 
       FileTypes = %w{
         application/pdf
+
         application/x-shockwave-flash
+
+        application/font-sfnt
+        application/x-font-opentype
+        application/x-font-otf
+        application/x-font-truetype
+        application/x-font-ttf
+
+        application/rtf
+
+        application/javascript
+        application/x-javascript
+        text/javascript
       }
 
     end
