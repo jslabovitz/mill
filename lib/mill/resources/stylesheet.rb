b3f3f56a7195ff3ffa431010ae09cd56634ebbba
John Labovitz <johnl@johnlabovitz.com>
2017-06-27 20:53:18 -0400

    Simplify SASS parsing.

--- lib/mill/resources/stylesheet.rb
+++ lib/mill/resources/stylesheet.rb
@@ -11,19 +11,8 @@ module Mill
       def load
         super
         unless @input_file.basename.to_s.end_with?('.min.css')
-          engine = Sass::Engine.new(@input_file.read,
-            filename: @input_file.to_s,
-            syntax: :scss,
-            style: :compressed)
+          engine = Sass::Engine.for_file(@input_file.to_s, style: :compressed)
           @content = engine.render
-          if false
-            original_size = @input_file.size
-            minified_size = @content.length
-            ;;warn "saved %d bytes (%.1f%%) by minifying CSS #{uri}" % [
-              original_size - minified_size,
-              (minified_size.to_f / original_size) * 100
-            ]
-          end
         end
       end
 
