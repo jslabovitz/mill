e317642ae6d6f4ffbce9f46227e944494945808b
John Labovitz <johnl@johnlabovitz.com>
2017-07-12 10:45:05 -0400

    Trap errors better when parsing CSS.

--- lib/mill/resources/stylesheet.rb
+++ lib/mill/resources/stylesheet.rb
@@ -12,7 +12,11 @@ module Mill
         super
         unless @input_file.basename.to_s.end_with?('.min.css')
           engine = Sass::Engine.for_file(@input_file.to_s, syntax: :scss, style: :compressed)
-          @content = engine.render
+          begin
+            @content = engine.render
+          rescue Sass::SyntaxError => e
+            raise "#{input_file}: error parsing CSS: #{e}"
+          end
         end
       end
 
