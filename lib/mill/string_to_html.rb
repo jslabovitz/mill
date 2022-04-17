d440768cb64a25702a0788db82109ccbd6a0740e
John Labovitz <johnl@johnlabovitz.com>
2022-04-17 12:46:42 +0200

    Add missing file.

---
+++ lib/mill/string_to_html.rb
@@ -0,0 +1,25 @@
+class String
+
+  Converters = {
+    nil => RubyPants,
+    smart_quotes: RubyPants,
+    markdown: Kramdown::Document,
+    textile: [RedCloth, :no_span_caps],
+  }
+
+  def to_html(options={})
+    converter_class = Converters[options[:mode]] or raise "Unknown to_html mode: #{options[:mode].inspect}"
+    if converter_class.kind_of?(Array)
+      converter_class, *converter_options = *converter_class
+      converter = converter_class.new(self, converter_options)
+    else
+      converter = converter_class.new(self)
+    end
+    html = Nokogiri::HTML::DocumentFragment.parse(converter.to_html)
+    if !options[:multiline] && (p_elem = html.at_xpath('p'))
+      html = p_elem.children.to_html
+    end
+    html.to_html
+  end
+
+end
